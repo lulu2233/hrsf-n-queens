ad1f7a01ab62dc08450a3b66dad92fd1d4a8ff5f
Minlu Jiang <ninglv111@gmail.com>
2020-02-17 17:30:48 -0800

    completed the n queens

--- src/Board.js
+++ src/Board.js
@@ -62,14 +62,19 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
     \__ \ || (_| | |  | |_  | | | |  __/ | |  __/_
     |___/\__\__,_|_|   \__| |_| |_|\___|_|  \___(_)
 
- */
+    //      _             _     _
+    //  ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
+    // / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
+    // \__ \ || (_| | |  | |_  | | | |  __/ | |  __/_
+    // |___/\__\__,_|_|   \__| |_| |_|\___|_|  \___(_)
+
     /*=========================================================================
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
@@ -79,12 +84,36 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      //creat myRow to store informatin in the row
+      var n = this.get('n');
+      var myRow = this.rows();
+      // console.log(myRow[rowIndex]);
+      // console.log('my row: ' + myRow);
+      //use the count to collect the number of queens
+      var count = 0;
+      for (var i = 0; i < n; i++) {
+        //check if the number of queens in one line larger than 1
+        //if yes has a row conflic
+        //if no continue
+        if (myRow[rowIndex][i] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -94,12 +123,29 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      var n = this.get('n');
+      var myCol = this.rows();
+      var count = 0;
+      for (var i = 0; i < n; i++) {
+        if (myCol[i][colIndex] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -108,12 +154,33 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMajorDiagonalConflictAt: function(row, majorDiagonalColumnIndexAtFirstRow) {
+      var n = this.get('n');
+      var board = this.rows();
+      var startCol = majorDiagonalColumnIndexAtFirstRow;
+      var startRow = row;
+      var count = 0;
+      for (var i = startRow, j = startCol; i < n && j < n; i++, j++) {
+        if (board[i][j] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      var n = this.get('n');
+      for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n - 1; j++) {
+          if (this.hasMajorDiagonalConflictAt(i, j)) {
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
@@ -123,14 +190,38 @@
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMinorDiagonalConflictAt: function(row, minorDiagonalColumnIndexAtFirstRow) {
+      //we can transpose the matrix
+      var n = this.get('n');
+      var board = this.rows();
+      var startCol = minorDiagonalColumnIndexAtFirstRow;
+      var startRow = row;
+      var count = 0;
+      for (var i = startRow, j = startCol; i < n && j >= 0; i++, j--) {
+        if (board[i][j] === 1) {
+          count++;
+        }
+        if (count > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
-    }
+      var n = this.get('n');
+      //we don't need to check the first minor diasgonals, because it only has one element
+      for (var i = 0; i < n; i++) {
+        for (var j = 1; j < n; j++) {
+          if (this.hasMinorDiagonalConflictAt(i, j)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    },
+
 
     /*--------------------  End of Helper Functions  ---------------------*/
 
--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,47 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //create a empty board
+  var solution = new Board({n: n});
+  //choose a start point
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      solution.togglePiece(i, j);
+      if (!solution.hasAnyRooksConflicts()) {
+        //jump out the inner loop
+        break;
+      } else {
+        //remove the rook at (i,j)
+        solution.togglePiece(i, j);
+      }
+    }
+  }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution.rows()));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({n: n});
+
+  var helper = function(row) {
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyRooksConflicts()) {
+        helper(row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+
+  helper(0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -32,15 +64,67 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n: n});
+  //choose a start point
+  var helper = function(row) {
+    if (row === n) {
+      return true;
+    }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts()) {
+        if (helper(row + 1)) {
+          return true;
+        }
+      }
+      board.togglePiece(row, i);
+    }
+    return false;
+  };
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  // for (var i = 0; i < n; i++) {
+  //   for (var j = 0; j < n; j++) {
+  //     solution.togglePiece(i, j);
+  //     if (!solution.hasAnyQueensConflicts()) {
+  //       //jump out the inner loop
+  //       break;
+  //     } else {
+  //       //remove the rook at (i,j)
+  //       solution.togglePiece(i, j);
+  //       //if we can't find a place to put a queen in this line,we need to back to the pre line
+  //       if (j === n) {
+
+  //       }
+  //     }
+  //   }
+  // }
+
+  helper(0);
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(board.rows()));
+  return board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n: n});
+
+  var helper = function(row) {
+    if (row === n) {
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row, i);
+      if (!board.hasAnyQueensConflicts() && board.rows()[row].indexOf(1) !== -1) {
+        helper(row + 1);
+      }
+      board.togglePiece(row, i);
+    }
+  };
+
+  helper(0);
+
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
